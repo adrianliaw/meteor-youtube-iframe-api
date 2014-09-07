b2b1cd346fe597b52067252449453c1a6c8871ed
adrianliaw <adrianliaw2000@gmail.com>
2014-09-07 17:46:05 +0800

    delete www-widgetapi.js

--- package.js
+++ package.js
@@ -1,13 +1,12 @@
 Package.describe({
   summary: "Youtube Iframe API",
-  version: "1.0.2",
+  version: "1.0.333",
   git: "http://github.com/adrianliaw/meteor-youtube-iframe-api.git"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.1.1');
   api.addFiles('lib/iframe_api.js', 'client');
-  api.addFiles('lib/www-widgetapi.js', 'client');
   api.export('YT', 'client');
   api.export('YTConfig', 'client');
 });
