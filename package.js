40a988dff8f28c6aabef56ac1994b1b2c1fa3480
adrianliaw <adrianliaw2000@gmail.com>
2014-10-06 14:27:10 +0800

    Works

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
   summary: "Youtube Iframe API",
-  version: "1.1.0",
+  version: "1.3.121",
   git: "http://github.com/adrianliaw/meteor-youtube-iframe-api.git"
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.1.1');
+  api.versionsFrom('METEOR@0.9.3.1');
+  api.use('jquery', 'client');
   api.addFiles('lib/iframe_api.js', 'client');
   api.export('YT', 'client');
   api.export('YTConfig', 'client');
